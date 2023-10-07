75aec5883f1131c6afba7321a003f628d990adaa
Victor <victoraalmeidac@gmail.com>
2023-10-07 20:00:05 -0300

    Concluindo segundo desafio.

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import modelo.Audio;
+import modelo.MinhasPreferidas;
+import modelo.Musica;
+import modelo.Podcast;
+public class Main {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
